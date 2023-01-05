dd262bac110ba73683fd36033dcdb2b5fbcdcb98
bobby <sirbob12@gmail.com>
2023-01-05 12:53:32 -0800

    new commit to the staging

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
 
-const connectDB = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGOURI, {
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-        console.log("MONGODB CONNECTION SUCCESS")
-    } catch (error) {
-        console.error("MongoDB failed");
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connection SUCCESS");
+  } catch (error) {
+    console.error("MongoDB connection FAIL");
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
- 
