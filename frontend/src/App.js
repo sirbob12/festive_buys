1e7b8a880d8d1a2531fa4008133223f8b124172e
bobby <sirbob12@gmail.com>
2023-01-06 12:22:08 -0800

    new modification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="heading_tag">This is a fullstack application!!</h1>
+      <h1 className="heading_tag">This is an ecommerce application!!!</h1>
+      <p className="paragraph__packages">
+        This is the fullstack application to give us 
+         better understanding of the dev concept..That we know
+          Is react that effective!!
+           Why is react the goto frontend framework for all 
+            frontend developers!!
+      </p>
     </div>
   );
 }
