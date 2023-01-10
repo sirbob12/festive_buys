9e7ff0af84e17fdae0ea87679361b7cb95d86879
bobby <sirbob12@gmail.com>
2023-01-10 11:08:05 -0800

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,11 @@
 
 import './App.css';
+import Header from './components/header/Header';
 
-function App() {
+function App() { 
   return (
     <div className="App">
-      <h1 className="heading_tag">This is an ecommerce application!!!</h1>
-      <p className="paragraph__packages">
-        This is the fullstack application to give us 
-         better understanding of the dev concept..That we know
-          Is react that effective!!
-           Why is react the goto frontend framework for all 
-            frontend developers!!
-      </p>
+      <Header/>
     </div>
   );
 }
